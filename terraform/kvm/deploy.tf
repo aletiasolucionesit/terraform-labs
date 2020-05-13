0787318161b156a012104bfd590d971d92b1d207
Jose David Martin Nieto <j.david.nieto@gmail.com>
2020-05-14 00:28:07 +0200

    optimized for terraform 12

--- terraform/kvm/deploy.tf
+++ terraform/kvm/deploy.tf
@@ -22,15 +22,15 @@ resource "libvirt_network" "network"{
 
 resource "libvirt_volume" "os_image" {
   name = "os_image_${var.purpose}"
-  source = "${var.image_id}"
-  pool = "${var.libvirt_pool}"
+  source = var.image_id
+  pool = var.libvirt_pool
 }
 
 resource "libvirt_volume" "volume-qcow2" {
   name = "${var.purpose}-tf-${count.index+1}.qcow2"
-  base_volume_id = "${libvirt_volume.os_image.id}"
-  count = "${var.machine_count}"
-  pool = "${var.libvirt_pool}"
+  base_volume_id = libvirt_volume.os_image.id
+  count = var.machine_count
+  pool = var.libvirt_pool
 }
 
 data "template_file" "user_data" {
@@ -47,21 +47,21 @@ data "template_file" "network_config" {
 # you can add also meta_data field
 resource "libvirt_cloudinit_disk" "commoninit" {
   name           = "commoninit.iso"
-  user_data      = "${data.template_file.user_data.rendered}"
-  network_config = "${data.template_file.network_config.rendered}"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
 }
 
 # Create the machine
 resource "libvirt_domain" "domain-machine" {
-  count = "${var.machine_count}"
+  count = var.machine_count
   name   = "${var.purpose}-tf-${count.index+1}"
-  memory = "${var.memory}"
-  vcpu   = "${var.vcpu}"
+  memory = var.memory
+  vcpu   = var.vcpu
 
-  cloudinit = "${libvirt_cloudinit_disk.commoninit.id}"
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
 
   network_interface {
-    network_name = "${libvirt_network.network.name}"
+    network_name = libvirt_network.network.name
     hostname       = "node-${count.index+1}.${var.purpose}.${var.base_domain}"
     addresses      = ["${var.base_ip}.2${count.index+1}"]
     mac            = "AA:BB:CC:11:22:2${count.index+1}"
@@ -84,7 +84,7 @@ resource "libvirt_domain" "domain-machine" {
   }
 
   disk {
-    volume_id = "${libvirt_volume.volume-qcow2.*.id[count.index]}"
+    volume_id = libvirt_volume.volume-qcow2.*.id[count.index]
   }
 
   graphics {
--- terraform/kvm/vars.tf
+++ terraform/kvm/vars.tf
@@ -1,40 +1,40 @@
 variable "image_id" {
-  type = "string"
+  type = string
   default = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
   # default = "/var/lib/libvirt/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
 }
 
 variable "purpose" {
-  type = "string"
+  type = string
   default = "test"
 }
 
 variable "machine_count" {
-  type = "string"
+  type = string
   default = "1"
 }
 
 variable "libvirt_pool"{
-  type = "string"
-  default = "vms"
+  type = string
+  default = "default"
 }
 
 variable "memory"{
-  type = "string"
+  type = string
   default = "2048"
 }
 
 variable "vcpu"{
-  type = "string"
+  type = string
   default = "1"
 }
 
 variable "base_domain"{
-  type = "string"
+  type = string
   default = "test"
 }
 
 variable "base_ip"{
-  type = "string"
+  type = string
   default = "192.168.100"
 }
