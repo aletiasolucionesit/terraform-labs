42ff1b85ca2bbf82082bdc319845927d0a041af8
Jose David Martin Nieto <j.david.nieto@gmail.com>
2021-02-02 23:03:13 +0100

    added new centos version

--- terraform/kvm/vars.tf
+++ terraform/kvm/vars.tf
@@ -1,7 +1,7 @@
 variable "image_id" {
   type = string
   default = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
-  # default = "/var/lib/libvirt/images/CentOS-8-GenericCloud-8.1.1911-20200113.3.x86_64.qcow2"
+  # default = "/var/lib/libvirt/images/CentOS-8-GenericCloud-8.3.2011-20201204.2.x86_64.qcow2"
 }
 
 variable "purpose" {
